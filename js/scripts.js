ecfcf1549c9c1bee8a8e2bf7a8989e48cba33c29
pascal <pnabwana13@gmail.com>
2019-02-23 22:09:33 +0300

    Added readme file

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,80 @@
-$("#forms").ready(function(){
-  $("input[type='button']").click(function(event){
+$(document).ready(function() {
+    $("input[type='button']").click(function(event) {
+      var ans1 = $("input[name='Quest1']:checked").val();
+      var ans2 = $("input[name='Quest2']:checked").val();
+      var ans3 = $("input[name='Quest3']:checked").val();
+      var ans4 = $("input[name='Quest4']:checked").val();
+      var ans5 = $("input[name='Quest5']:checked").val();
+      var ans6 = $("input[name='Quest6']:checked").val();
+      var ans7 = $("input[name='Quest7']:checked").val();
+      var ans8 = $("input[name='Quest8']:checked").val()
+    //   var answers = ["A","B","A","B","B","C","C","C" ],
+      var score = 0;
+      if (ans1 === "A")
+      {
+      score += 10;
+      }
+        else
+        {
+        score += 0;
+        }
+      if (ans2 === "A")
+      {
+        score += 10;
+      }
+        else
+        {
+        score += 0;
+        }
+      if (ans3 === "B")
+      {
+      score += 10;
+      }
+        else
+        {
+          score += 0;
+         }
+      if (ans4 === "B")
+      {
+      score += 10;
+      }
+       else
+       {
+       score += 0;
+       }
+      if (ans5 === "B")
+      {
+      score += 10;
+      }
+       else
+        {
+        score += 0;
+        }
+      if (ans6 === "C")
+      {
+      score += 10;
+      }
+        else
+        {
+        score += 0;
+        }
+      if (ans7 === "C")
+      {
+      score += 10;
+      }
+      else
+      {
+        score += 0;
+      }
+      if (ans8 ==="C")
+      {
+      score += 10;
+    }
+    else
+      {
+        score += 0;
+    }
+    $("#result").text("You have scored "+score+" out of 80");
 
-    var Question1 = $("question1:checked").val();
-    var Question2= $("question2:checked").val();
-    var Question3 = $("question3:checked").val();
-    var Question4 = $("question4:checked").val();
-
-    var total=0;
-
-    if (Question1==="c"){
-      total+=20;
-    }
-    else {
-    total+=0;
-    }
-
-    if (Question2==="c"){
-      total+=20;
-    }
-    else {
-    total+=0;
-    }
-
-    if (Question3==="c"){
-      total+=20;
-    }
-    else {
-    total+=0;
-    }
-
-    if (Question4==="c"){
-      total+=20;
-    }
-    else {
-    total+=0;
-    }
-    $(".result").text("your score is " + total);
-event.preventDefault();
-  });
+    });
 });
